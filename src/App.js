abf8de8b6fa89f20023314e78281dcd0d5197cda
kareemelhosseny <k.elhosseny@gmail.com>
2020-10-23 01:28:27 +0200

    mit Rotuing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from "react";
-import "./App.css";
+import { Route, Switch, Redirect } from "react-router-dom";
+import NavBar from "./components/common/navbar";
+import Home from "./components/Home";
 import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import MovieForm from "./components/movieForm";
+import "./App.css";
 function App() {
   return (
-    <main role="main" className="container">
-      <Movies></Movies>
-    </main>
+    <React.Fragment>
+      <NavBar />
+      <main role="main" className="container">
+        <Switch>
+          <Route path="/movies/:id" component={MovieForm} />
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/home" component={Home} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect from="/" exact to="/home" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
